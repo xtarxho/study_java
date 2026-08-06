ed20b889eb62ebebfa63adcc677391582fe9062a
xtarxho <starcho12@naver.com>
2026-08-06 17:38:19 +0900

    0806 수업내용

--- ex_0806_csm/src/ex_work/Ex1_work.java
+++ ex_0806_csm/src/ex_work/Ex1_work.java
@@ -35,9 +35,65 @@ public class Ex1_work {
         System.out.println(res);
 */
         //내가 만든 삼항연산자 
-        String  res2 = (( age >= 15 || cm >= 150) ? "탑승가능" : "탑승불가" );
+        String  res2 = ( age >= 15 || cm >= 150 ) ? "탑승가능" : "탑승불가" ;
 
         System.out.println(res2);
+
+
+
+
+        // [문제1] 시험 점수가 60점 이상이거나, 과제 점수가 80점 이상이면 "합격"을 출력하고,
+        // 둘 다 해당되지 않으면 "불합격"을 출력하는 코드를 작성해보세요.
+
+        int test = 46;
+        int homework = 84;
+        String str = "";
+
+        if( test >= 60 || homework >=80 ){
+            str = "합격";
+        }else{
+            str = "불합격";
+        }
+
+        System.out.println( str );
+
+
+
+        // [문제2] 나이가 65세 이상이거나, 장애인등록증이 있으면 "교통비 무료"를 출력,
+        // 아니면 "교통비 유료"를 출력하는 코드를 만들어보세요.
+
+        int age1 = 76;
+        boolean card = true;
+        String ggg = "";
+
+        if( age1 >= 65 || card == false ){
+            ggg = "교통비 무료";
+        }else{
+            ggg = "교통비 유료";
+        }
+
+        System.out.println(ggg);
+
+
+
+
+        // [문제3] 회원 등급이 "VIP"이거나, 누적 구매금액이 100만원 이상이면 "할인 적용",
+        // 아니면 "할인 없음"을 출력하는 조건문을 작성해보세요.
+
+        String a = "vvip";
+        int b = 500000;
+        String str2 = "";
+
+        if( a == "vip"  ||  b >= 1000000 ){
+            str2 = "할인 적용";
+        }else{
+            str2 = "할인 없음";
+        }
+
+
+        System.out.println(str2);
+
+
 
 
 
