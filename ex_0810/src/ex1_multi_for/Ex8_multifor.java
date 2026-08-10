a428db0acc41f2c55cf845ee4d884aa287962a09
xtarxho <starcho12@naver.com>
2026-08-10 17:38:38 +0900

    0810 수업내용

--- ex_0810/src/ex1_multi_for/Ex8_multifor.java
+++ ex_0810/src/ex1_multi_for/Ex8_multifor.java
@@ -2,27 +2,33 @@ package ex1_multi_for;
 
 public class Ex8_multifor {
     public static void main(String[] args) {
-        
+
         /*
-        1 2 3 4 5 6 7 8 9 10
-        2 3 4 5 6 7 8 9 10 1
-        3 4 5 6 7 8 9 10 1 2
+         * 1 2 3 4 5 6 7 8 9 10
+         * 2 3 4 5 6 7 8 9 10 1
+         * 3 4 5 6 7 8 9 10 1 2
+         * 
+         * .........
+         * 10 1 2 3 4 5 6 7 8 9
+         */
 
-        .........
-        10 1 2 3 4 5 6 7 8 9
-        */
+        for (int i = 1; i <= 10; i++) {
 
-        for( int i = 1; i <= 10; i++ ){
-
-            for( int j = 1; j <= 10; j++ ){
-
-
-            }//inner
-
-        }//outer
+            for (int j = 1; j <= 10; j++) {
+                
+                if( i <= j && j <= 10 ){
+                    System.out.print(j);
+                    
+                }else if( j >= 10 ){
+                    
+                }
 
 
+            } // inner
+            System.out.println();
+       
+        } // outer
 
+    }
 
-    }//main
-}
+}// main
--- ex_0810/src/ex1_multi_for/Ex9_multifor.java
+++ ex_0810/src/ex1_multi_for/Ex9_multifor.java
@@ -13,5 +13,24 @@ public class Ex9_multifor {
 
 */
 
+
+        for(int i = 1; i <= 5; i++){
+
+            for( int j = 1; j <= 9; j++ ){
+                if( i % 2 == 1 ){
+                    System.out.print("*");
+                }
+
+
+            }
+            System.out.println();
+        }
+
+
+
+
+
+
+
     }//main
 }
