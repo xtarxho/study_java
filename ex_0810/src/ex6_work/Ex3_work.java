d81808e502159051b15c084176fc3bb12ae7d318
xtarxho <starcho12@naver.com>
2026-08-27 17:37:07 +0900

    0810 수업내용

--- ex_0810/src/ex6_work/Ex3_work.java
+++ ex_0810/src/ex6_work/Ex3_work.java
@@ -12,29 +12,6 @@ public class Ex3_work {
         //6 : 1 5  2 4  3 3  4 2  5 1
 
      
-        //틀림 집가서 복습
-        for( int i = 1; i <= 6; i++ ){
-
-            for( int j = 1; j <= 6; j++ ){
-
-                if (i + j == 5) {
-                    System.out.print("5 : " + i + j);
-                    
-                }else if( i + j == 6 ){
-                    System.out.print( "6 :  " + i + j);
-                }else if( i + j == 7 ){
-                    System.out.println( "7 :  " + i + j);
-                }
-
-
-
-            }
-
-            
-            
-        }
-        System.out.println("================================");
-
         
         
         String five = "";
